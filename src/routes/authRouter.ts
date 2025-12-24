29369de8024bede3e5f8cfaa312a2611354e3e68
Mateo-sotnyk <sotnykmateo@gmail.com>
2025-12-24 00:45:31 -0300

    add: limites en rutas de auth

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
 import AuthController from "../controllers/authController"
+import rateLimitMiddleware from "../middleware/rateLimitMiddleware"
 
 const authRouter = Router()
+
+authRouter.use(rateLimitMiddleware)
 
 // http://localhost:3000/auth/register
 authRouter.post("/register", AuthController.register)
